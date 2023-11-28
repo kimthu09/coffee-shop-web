d65e2146486a8dc512c0bc812398003937840f37
kimthu09 <21521495@gm.uit.edu.vn>
2023-11-28 15:05:55 +0700

    dev: UI layout and constants

--- coffee_shop_management/types/index.ts
+++ coffee_shop_management/types/index.ts
@@ -1,50 +1,112 @@
+import { IconType } from "react-icons";
+
 export type Product = {
   id: string;
   name: string;
   price: number;
   status: "active" | "inactive";
   image?: string;
+  idCate?: string;
 };
 
-export const products: Product[] = [
-  {
-    id: "ABC123",
-    name: "Tra sua",
-    price: 15000,
-    status: "active",
-    image:
-      "https://img.freepik.com/free-photo/cold-coffee-drink_144627-18369.jpg?w=740&t=st=1697954121~exp=1697954721~hmac=5b1b188e7f2cb863d08f826a31f99074fe923b6371119e1c652037a1458ef27d",
-  },
-  {
-    id: "AEF355",
-    name: "Ca phe",
-    price: 17000,
-    status: "active",
-    image:
-      "https://img.freepik.com/free-photo/cold-coffee-drink_144627-18369.jpg?w=740&t=st=1697954121~exp=1697954721~hmac=5b1b188e7f2cb863d08f826a31f99074fe923b6371119e1c652037a1458ef27d",
-  },
-  {
-    id: "ABC243",
-    name: "Sandwich",
-    price: 32000,
-    status: "inactive",
-    image:
-      "https://img.freepik.com/free-photo/cold-coffee-drink_144627-18369.jpg?w=740&t=st=1697954121~exp=1697954721~hmac=5b1b188e7f2cb863d08f826a31f99074fe923b6371119e1c652037a1458ef27d",
-  },
-  {
-    id: "GHJ123",
-    name: "Sua tuoi",
-    price: 12000,
-    status: "active",
-    image:
-      "https://img.freepik.com/free-photo/cold-coffee-drink_144627-18369.jpg?w=740&t=st=1697954121~exp=1697954721~hmac=5b1b188e7f2cb863d08f826a31f99074fe923b6371119e1c652037a1458ef27d",
-  },
-  {
-    id: "AFH123",
-    name: "Sua chua",
-    price: 18000,
-    status: "active",
-    image:
-      "https://img.freepik.com/free-photo/cold-coffee-drink_144627-18369.jpg?w=740&t=st=1697954121~exp=1697954721~hmac=5b1b188e7f2cb863d08f826a31f99074fe923b6371119e1c652037a1458ef27d",
-  },
-];
+export type SidebarItem = {
+  title: string;
+  href: string;
+  icon?: IconType;
+  submenu?: boolean;
+  subMenuItems?: SidebarItem[];
+};
+
+export type Category = {
+  id: string;
+  name: string;
+  quantity: number;
+};
+
+export type MeasureUnit = {
+  id: string;
+  name: "g" | "kg" | "l" | "ml" | "đơn vị";
+  covertDetails?: {
+    measureUnit: MeasureUnit;
+    times: number;
+  };
+};
+
+export type IngredientForChoose = {
+  id: string;
+  name: string;
+  unitId: string;
+};
+
+export type Ingredient = {
+  id: string;
+  name: string;
+  total: number;
+  unit: MeasureUnit;
+  price: number;
+};
+export type IngredientDetail = {
+  idIngre: string;
+  expirationDate: Date;
+  quantity: number;
+};
+
+export type Staff = {
+  id: string;
+  name: string;
+  role: string;
+};
+export type Role = {
+  id: string;
+  name: string;
+  function?: string[];
+};
+export type RoleFunction = {
+  id: string;
+  name: string;
+};
+
+export enum StatusString {
+  Inprogress = "Đang xử lý",
+  Done = "Đã nhập",
+  Cancel = "Đã huỷ",
+}
+export type ImportNote = {
+  id: string;
+  supplierId: string;
+  totalPrice: number;
+  status: StatusString;
+  createBy: string;
+  closeBy?: string;
+  createAt: Date;
+  closeAt?: Date;
+};
+export type ExportNote = {
+  id: string;
+  createBy: string;
+  createAt: Date;
+  reason: string;
+};
+
+export type Customer = {
+  id: string;
+  name: string;
+  email?: string;
+  phone: string;
+  point: number;
+};
+
+export interface UnitListProps {
+  unit: string;
+  setUnit: (unit: string) => void;
+}
+export interface RoleListProps {
+  role: string;
+  setRole: (role: string) => void;
+}
+
+export interface CategoryListProps {
+  category: string;
+  setCategory: (category: string) => void;
+  canAdd?: boolean;
+}
