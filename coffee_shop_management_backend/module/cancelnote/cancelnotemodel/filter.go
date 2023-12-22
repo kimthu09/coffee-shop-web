c519001a478af03caccc375f5aa54990f7bd0082
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2023-12-22 19:18:20 +0700

    update: fix code and add more test for cancel note

--- coffee_shop_management_backend/module/cancelnote/cancelnotemodel/filter.go
+++
@@ -1,7 +0,0 @@
-package cancelnotemodel
-
-type Filter struct {
-	SearchKey string   `json:"searchKey,omitempty" form:"search"`
-	MinPrice  *float32 `json:"minPrice,omitempty" form:"minPrice"`
-	MaxPrice  *float32 `json:"maxPrice,omitempty" form:"maxPrice"`
-}
--- coffee_shop_management_backend/module/cancelnotedetail/cancelnotedetailmodel/filter.go
+++
@@ -1,4 +0,0 @@
-package cancelnotedetailmodel
-
-type Filter struct {
-}
