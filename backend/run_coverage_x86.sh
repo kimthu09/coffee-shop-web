66e74b9d02acade6e32fbb469cc657a84ac24ba5
Bui Vi Quoc <bvquoc.dev@gmail.com>
2023-12-25 16:22:19 +0700

    update run script & report folder

--- backend/run_coverage_x86.sh
+++ backend/run_coverage_x86.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 go test ./... -coverprofile=c.out
-./tools/x86/gocov convert c.out | ./tools/x86/gocov-html >cov_report.html
+./tools/x86/gocov convert c.out | ./tools/x86/gocov-html >report/cov_report.html
 rm -rf ./c.out
-COV_REPORT_FILE=./cov_report.html
+COV_REPORT_FILE=./report/cov_report.html
 if test -f "$COV_REPORT_FILE"; then
     open $COV_REPORT_FILE
 fi
--- backend/run_test.sh
+++ backend/run_test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 go test ./... -json -v | ./tools/x86/go-test-report -p -t "Coffee Shop Management - Test Report"
 
-TEST_REPORT_FILE=./test_report.html
+rm ./report/test_report.html
+mv ./test_report.html ./report/test_report.html
+
+TEST_REPORT_FILE=./report/test_report.html
 if test -f "$TEST_REPORT_FILE"; then
     open $TEST_REPORT_FILE
 fi
