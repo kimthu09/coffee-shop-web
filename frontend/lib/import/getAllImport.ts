6c409de39eb0fe17e3c67e24c82f2398e5059cd1
kimthu09 <21521495@gm.uit.edu.vn>
2023-12-25 17:56:12 +0700

    fix: import note ui

--- frontend/lib/import/getAllImport.ts
+++ frontend/lib/import/getAllImport.ts
@@ -1,32 +1,35 @@
-import { apiKey, endPoint } from "@/constants";
+import { endPoint } from "@/constants";
 import useSWR from "swr";
-
-const fetcher = (url: string) =>
-  fetch(url, {
-    headers: {
-      accept: "application/json",
-      Authorization: apiKey,
-    },
-  })
-    .then((res) => {
-      return res.json();
-    })
-    .then((json) => {
-      return {
-        paging: json.paging,
-        data: json.data,
-      };
-    });
+import { getToken } from "../auth";
 
 export default function getAllImportNote({
   page,
   limit,
   filterString,
+  token,
 }: {
   page: string;
   limit?: number;
   filterString?: string;
+  token: string;
 }) {
+  const fetcher = (url: string) => {
+    return fetch(url, {
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        return {
+          paging: json.paging,
+          data: json.data,
+        };
+      });
+  };
   const { data, error, isLoading, mutate, isValidating } = useSWR(
     `${endPoint}/importNotes?page=${page}&limit=${limit ?? "10"}${
       filterString ?? ""
